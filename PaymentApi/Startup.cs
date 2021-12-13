a49d1ba8164a3f11194c482d779ee070fec2c7bc
ikamuliana <ikamulianad@gmail.com>
2021-12-13 19:44:09 +0700

    update cors

--- PaymentApi/Startup.cs
+++ PaymentApi/Startup.cs
@@ -31,7 +31,12 @@ namespace PaymentApi
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) 
+        {
+            this.Configuration = configuration;
+               
+        }
+                public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -110,6 +115,11 @@ namespace PaymentApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseAuthorization();
 
